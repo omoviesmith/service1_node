f7ce5cef29dd98fbebf9e6501fe47e133db50679
Smith Omovie <omoviesmith@gmail.com>
2024-05-01 01:09:29 +0100

    updated package.json

--- index.js
+++ index.js
@@ -2,9 +2,11 @@
 
 var path = require('path');
 var http = require('http');
+var cors = require('cors'); // Require the CORS package
 
 var oas3Tools = require('oas3-tools');
-var serverPort = 5000;
+const { FAILSAFE_SCHEMA } = require('js-yaml');
+var serverPort = 8080;
 
 // swaggerRouter configuration
 var options = {
@@ -16,9 +18,15 @@ var options = {
 var expressAppConfig = oas3Tools.expressAppConfig(path.join(__dirname, 'api/openapi.yaml'), options);
 var app = expressAppConfig.getApp();
 
+app.use(cors({
+    origin: 'http://localhost:5000', // Adjust according to where your client is hosted
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: false
+})); // Use CORS middleware to enable CORS
+
 // Initialize the Swagger middleware
 http.createServer(app).listen(serverPort, function () {
     console.log('Your server is listening on port %d (http://localhost:%d)', serverPort, serverPort);
     console.log('Swagger-ui is available on http://localhost:%d/docs', serverPort);
 });
-
